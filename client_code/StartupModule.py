f5120c8af2cbcc001b943c0a2968a8cbcd645bc5
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-03-05 06:18:10 +0000

    Edited StartupModule and Frame

--- client_code/StartupModule.py
+++ client_code/StartupModule.py
@@ -8,9 +8,9 @@ from anvil.tables import app_tables
 from anvil import *
 
 def startup():
-  user = anvil.users.login_with_form()
-  if not user['color']:
-    anvil.server.call('add_user_color')
-  open_form("Frame")
+    user = anvil.users.login_with_form()
+    if not user['color']:
+        anvil.server.call('add_user_color')
+    open_form("Frame")
 
 startup()
