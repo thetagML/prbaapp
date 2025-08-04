0c0e67e159df8776d9724e3479c7df4cb5d82277
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-08-04 19:43:18 +0000

    Edited Frame

--- client_code/Frame/__init__.py
+++ client_code/Frame/__init__.py
@@ -7,6 +7,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..ProjectView import ProjectView
 from ..ProjectList import ProjectList
+import anvil.js
+from anvil.js.window import jQuery
 
 class Frame(FrameTemplate):
     def __init__(self, **properties):
@@ -15,6 +17,21 @@ class Frame(FrameTemplate):
         #self.content_panel.add_component(ProjectList())
         #self.add_project_links()
 
+        jQuery( "#anvil-header" ).remove()
+
+    def form_show(self, **event_args):
+      """This method is called when the HTML panel is shown on the screen"""
+      jQuery( ".app-bar" ).css( "top", "0px" )
+      jQuery( ".content" ).css( "margin-top", "9px" )
+      jQuery( ".sidebar-toggle" ).css( "margin-top", "0px" )
+      jQuery( ".title.anvil-inline-container div" ).css( "margin-top", "0px" )
+
+
+      #move to the bottom
+      el = anvil.js.get_dom_node( self.create_new )
+      el.parentElement.parentElement.parentElement.parentElement.parentElement.style.position = "absolute"
+      el.parentElement.parentElement.parentElement.parentElement.parentElement.style.bottom = "0"
+  
     def add_project_links(self):
         self.link_panel.clear()
         for row in anvil.server.call('get_projects'):
@@ -50,7 +67,7 @@ class Frame(FrameTemplate):
                 link.role = "selected"
         self.open_project(project)
 
-    def projects_menu_click(self, sender, **event_args):
+    def desktops_menu_click(self, sender, **event_args):
         """This method is called when the link is clicked"""
         for link in self.link_panel.get_components():
             link.role = ""
